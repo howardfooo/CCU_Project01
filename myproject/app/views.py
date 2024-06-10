4a414675f983c82a31d22a1752826677dd27a4f6
howardfooo <howardfooo@gmail.com>
2024-06-10 21:24:21 +0800

    連接資料庫前之預備

--- myproject/app/views.py
+++ myproject/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse,redirect
 from django.http import JsonResponse
 from datetime import datetime
 from django.db import connection
@@ -25,6 +25,55 @@ def login(request):
 def signup(request):
     return render(request, "signup.html")
 
+def map_(request):
+    mycursor.execute('select * from Restaurant')
+    restaurants_info = mycursor.fetchall()
+    
+    restaurants = []
+    restaurant_head = [head[0] for head in mycursor.description]
+    for restaurant_info in restaurants_info:
+        merge = zip(restaurant_head, restaurant_info)
+        restaurants.append(dict(merge))
+
+    return render(request, "map.html", {"restaurants": restaurants,})
+
+def restaurant_(request):
+    Rid = request.GET['Rid']
+    mycursor.execute('select * from Restaurant where Rid = %s', (Rid,))
+    restaurant_info = mycursor.fetchall()[0]
+    restaurant_head = [head[0] for head in mycursor.description]
+
+    merge = zip(restaurant_head, restaurant_info)
+    restaurant = dict(merge)
+
+    return render(request, "restaurant.html", {"restaurant": restaurant,})
+
+def comments_(request):
+    comment_range = range(5)
+
+    # restaurant
+    Rid = request.GET['Rid']
+    mycursor.execute('select * from Restaurant where Rid = %s', (Rid,))
+    restaurant_info = mycursor.fetchall()[0]
+    restaurant_head = [head[0] for head in mycursor.description]
+
+    merge = zip(restaurant_head, restaurant_info)
+    restaurant = dict(merge)
+
+    # comment
+    mycursor.execute('select * from Comment where Rid = %s', (Rid,))
+    comments_info = mycursor.fetchall()
+    comment_head = [head[0] for head in mycursor.description]
+
+    comments = []
+    for comment_info in comments_info:
+        merge = zip(comment_head, comments_info)
+        comments.append(dict(merge))
+
+    return render(request, "comments.html", {"comments": comments, "restaurant": restaurant,})
+
+
+
 def signupadd(request):
     username = request.POST['username']
     email = request.POST['email']
@@ -32,16 +81,17 @@ def signupadd(request):
     pwdcf = request.POST['pwdcf']
     print(username, email, pwd, pwdcf)
     mycursor.execute('insert into userinfo(userID, username, password, truename) values(%s, %s, %s, %s)',
-                     (2, username, pwd, username))
+                    (2, username, pwd, username))
     return render(request, "signupadd.html")
 
 def userlist(request):
-    mycursor.execute('select * from userinfo')
-    usersinfo = mycursor.fetchall()
-    head = [head[0] for head in mycursor.description]
+    mycursor.execute('select * from userinfo limit 1')
+    users_info = mycursor.fetchall()
+    user_head = [head[0] for head in mycursor.description]
     users = []
-    for user in usersinfo:
-        output = zip(head, user)
-        # print(dict(output))
-        users.append(dict(output))
+    # print(users_info)
+    for user in users_info:
+        merge = zip(user_head, user)
+        users.append(dict(merge))
+    print(users)
     return render(request, "userlist.html", {'userlist': users, 'head': head})
